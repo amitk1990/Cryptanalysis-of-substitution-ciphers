ed52004bef9075d775f4806d077eb362c3409e17
Amit Karunakaran <akarunakaran@shutterstock.com>
2015-10-10 19:20:45 -0400

    shift cipher done

--- src/shift.cpp
+++ src/shift.cpp
@@ -2,16 +2,63 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <vector>
 #include <map>
 
 using namespace std;
 string word;	// returns one word at a time
-map<string,int > dictionary2Map;
+map<string,int > dictionary2;
 string cipherText;
 int keys;
 char alphabetsArray[] = {' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}; 
+string dictionary1 = "";
 
-void shiftCipher(string cipherText){
+
+/*
+ * split string to words delimitor whitespace
+*/
+
+
+bool presentInDictionary2(string substr){
+	string buf;
+	stringstream ss(substr);
+	std::vector<string> tokens;
+	while(ss >> buf){
+		tokens.push_back(buf);
+	}
+	for (std::vector<string>::const_iterator i = tokens.begin(); i != tokens.end(); ++i){
+    		//std::cout << *i << ' ';
+    		if(dictionary2.find(*i) == dictionary2.end()){
+    			return false;
+    		}
+	}
+	return true;
+	
+}
+/*
+* check if present in dictionary 1
+*/
+bool presentInDictionary1(string substr){
+	if(dictionary1.find(substr) != std::string::npos){
+		return true;
+	}
+	return false;
+}
+/*
+* read from dictionary to a string
+*/
+void readDicToString(){
+	char ch;
+
+	fstream fin("./../bin/Dictionary1.txt", fstream::in);
+	while (fin >> noskipws >> ch) {
+		dictionary1 +=ch;
+	}
+	//cout << dictionary1;
+}
+
+// shift cipher logic
+bool shiftCipher(string cipherText){
 	for(int i=1;i<=27;i++){
 		std::string plain_text="";
 		std::stringstream ss ;
@@ -20,19 +67,30 @@ void shiftCipher(string cipherText){
 			int after_index = (index + i)% 27;
 			plain_text+=alphabetsArray[after_index];
 		}
-		cout << plain_text << endl;
+		//cout << "**" << plain_text << "**" << endl;
+		if(presentInDictionary2(plain_text)){
+			cout << "present in dictionary2" << endl;
+			return true;
+
+		}else if(presentInDictionary1(plain_text)){
+				return true;
+		}
 	}
+	return false;
 
 }
+// display hash map
 void printHashMap(map<string,int > dictionary){
-	cout << "Map Size " << dictionary.size() << endl;
+	//cout << "Map Size " << dictionary.size() << endl;
 	for (std::map<string,int>::iterator it = dictionary.begin(); it != dictionary.end(); it++) { // iterate over the entries  
 	    string key = it->first;  // Get the key  
 	    int value = it->second;  // Get the value  
-	    std::cout << "(" << key << "," << value << ")" << std::endl;  
+	    //std::cout << "(" << key << "," << value << ")" << std::endl;  
 	} 
 }
 
+
+// read from dictionary to hash map
 void MapDictionary2(){
 	ifstream fin("./../bin/Dictionary2.txt");
 	if(!fin){
@@ -41,24 +99,28 @@ void MapDictionary2(){
 	}
 	while( fin >> word){
 		// cout << word << ": " << word.size() << endl;
-		dictionary2Map.insert(std::make_pair(word,word.size()));
+		dictionary2.insert(std::make_pair(word,word.size()));
 	}
-	// printHashMap(dictionary2Map); 
+	// printHashMap(dictionary2); 
 
 }
 
 
 
-int main(){
 
+/*
+* Starter function
+*/
+
+int main(){
+	readDicToString();
 	MapDictionary2();
 	cout << "Enter the cipher text " << endl;
 	getline(cin,cipherText);
-	cout << cipherText << endl;
+	int len = cipherText.length();
+	if(shiftCipher(cipherText))
+		cout << "present" << endl;
 	// cout << " Enter the number of keys used " << endl;
 	// cin >> keys;
-	int len = cipherText.length();
-	//cout << len;
-	shiftCipher(cipherText);
 }
 
