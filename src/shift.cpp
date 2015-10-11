60afd1354aa051d7f3ee6f58c77c79ddf70acd9a
Amit Karunakaran <akarunakaran@shutterstock.com>
2015-10-11 00:39:43 -0400

    poly substitution started

--- src/shift.cpp
+++ src/shift.cpp
@@ -11,21 +11,50 @@
 #include <sstream>
 #include <vector>
 #include <map>
+#include <set>
 
 using namespace std;
 string word;	// returns one word at a time
 map<string,int > dictionary2;
+std::set< std::vector<int> > outerVector;
 string cipherText;
 int keys;
 char alphabetsArray[] = {' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}; 
 string dictionary1 = "";
 
+/*
+ * Combination problem
+*/
+int comb[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26}; 
+long choose(int * got, int n_chosen, int len, int at, int max_types)
+{
+        int i;
+        long count = 0;
+        if (n_chosen == len) {
+                if (!got) return 1;
+ 				std::vector<int> innerVector;
+                for (i = 0; i < len; i++){
+                        //printf("%d ", comb[got[i]]);
+                        innerVector.push_back(comb[got[i]]);
+                }
+                outerVector.insert(innerVector);
+                cout << "size of outerVector" << outerVector.size() << endl;
+                printf("\n");
+                return 1;
+        }
+ 
+        for (i = at; i < max_types; i++) {
+                if (got) got[n_chosen] = i;
+                count += choose(got, n_chosen + 1, len, i, max_types);
+        }
+        return count;
+}
+
+
 
 /*
- * check to see present in dictionary2
+ * Check to see present in dictionary2
 */
-
-
 bool presentInDictionary2(string substr){
 	string buf;
 	stringstream ss(substr);
@@ -42,8 +71,8 @@ bool presentInDictionary2(string substr){
 	return true;
 	
 }
-/*
-* check if present in dictionary 1
+/* 
+ * Check if present in dictionary 1
 */
 bool presentInDictionary1(string substr){
 	if(dictionary1.find(substr) != std::string::npos){
@@ -52,7 +81,7 @@ bool presentInDictionary1(string substr){
 	return false;
 }
 /*
-* read from dictionary to a string
+ * Read from dictionary to a string
 */
 void readDicToString(){
 	char ch;
@@ -131,16 +160,29 @@ void MapDictionary2(){
 int main(){
 	readDicToString();
 	MapDictionary2();
-	cout << "ENTER THE CIPHER TEXT TO BE DECODED " << endl;
-	getline(cin,cipherText);
-	// cout << " Enter the number of keys used " << endl;
-	// cin >> keys;
-	if(shiftCipher(cipherText)){
-		cout << "PRESENT USING SHIFT CIPHER" << endl;
-	}else{
-		cout << "NOT PRESENT" << endl;
-	}
-
+	//cout << "ENTER THE CIPHER TEXT TO BE DECODED " << endl;
+	//getline(cin,cipherText);
+	 cout << " Enter the number of keys used " << endl;
+	 cin >> keys;
+	// if(shiftCipher(cipherText)){
+	// 	cout << "PRESENT USING SHIFT CIPHER" << endl;
+	// }else{
+	// 	cout << "NOT PRESENT" << endl;
+	// }
+	long value = choose(comb,0,3,0,3);
+	cout << value << endl;
+	std::set< std::vector<int> > ::iterator it;
+	std::cout << "**";
+ 	for(it = outerVector.begin(); it!=outerVector.end(); it++)
+ 	{
+ 		const std::vector<int>& i = (*it);
+ 		for(int j = 0; j<i.size(); j++)
+ 		{
+ 			cout << i[j] << " ";
+ 		}
+ 		cout << endl;
+ 	}
+	std::cout << "**" << endl;
 	
 }
 
